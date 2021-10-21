2b58c98d1058bc25018f5fc1353b16afa2cfe73e
Matthew Reishus <mreishus@users.noreply.github.com>
2021-10-21 16:41:34 -0500

    Challenge 16 complete!

--- set1/c16.py
+++ set1/c16.py
@@ -15,6 +15,7 @@ def enc(input_bytes):
 
 def is_admin(encrypted_bytes):
     full_bytes = cbc_decrypt(encrypted_bytes, mykey(), b"\x00" * 16)
+    print("    (The system has decoded the cipher text: )", full_bytes)
     return b";admin=true;" in full_bytes
 
 def mykey():
@@ -25,11 +26,36 @@ def mykey():
 
 def main():
     print("c16")
-    ct = enc(b"myinputdata=here=test1234;here==")
-    print("Cipher Text:")
+    print("I am sending this plaintext to the function:")
+    bytes_in = b"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+    print(bytes_in)
+    ct = enc(bytes_in)
+    print("")
+    print("I got this encrypted text back: ")
     print(ct)
-    print("Is admin? (Purpose of the exercise is to make this true)")
+    ct2 = bytearray(ct)
+    ct2[16] ^= (ord(';') ^ ord('a'))
+    ct2[17] ^= (ord('a') ^ ord('a'))
+    ct2[18] ^= (ord('d') ^ ord('a'))
+    ct2[19] ^= (ord('m') ^ ord('a'))
+    ct2[20] ^= (ord('i') ^ ord('a'))
+    ct2[21] ^= (ord('n') ^ ord('a'))
+    ct2[22] ^= (ord('=') ^ ord('a'))
+    ct2[23] ^= (ord('t') ^ ord('a'))
+    ct2[24] ^= (ord('r') ^ ord('a'))
+    ct2[25] ^= (ord('u') ^ ord('a'))
+    ct2[26] ^= (ord('e') ^ ord('a'))
+    ct2[27] ^= (ord(';') ^ ord('a'))
+    print("")
+    print("I will make bit flipping attacks to change the cyphertext without knowing the key: ")
+    print("Modified cyphertext: ")
+    print(bytes(ct2))
+    print("")
+    print("Checking to see if the system thinks the original ciphertext belongs to an admin:")
     print(is_admin(ct))
+    print("")
+    print("Checking to see if the system thinks the modified ciphertext belongs to an admin:")
+    print(is_admin(ct2))
 
 if __name__ == "__main__":
     main()
